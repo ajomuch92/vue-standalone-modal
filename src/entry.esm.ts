337252943e07967002dc1efe4600814cdafc435d
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-12-31 14:56:30 -0600

    feat: 🎸 Updating installation files

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -1,25 +1,32 @@
-import _Vue, { PluginObject } from 'vue';
+import Vue, { PluginFunction, PluginObject } from 'vue';
+import VueStandaloneModal from './vue-standalone-modal.vue'
+import { ModalProps } from './interfaces';
 
-// Import vue component
-import component from '@/vue-standalone-modal.vue';
+const showModal = (options: ModalProps) => {
+  const { component, parent, withCard, showCloseButton, events, props, innerClass, innerStyle } = options;
+  if (!component || !parent) throw new Error('No child or parent component provided');
+  const Modal = Vue.extend(VueStandaloneModal);
+  const modalInstance = new Modal({
+    parent,
+    propsData: {
+      component,
+      withCard,
+      showCloseButton,
+      innerClass,
+      innerStyle,
+      events,
+      props
+    }
+  });
+  modalInstance.$mount();
+  modalInstance.$data.visible = true;
+  parent.$el.appendChild(modalInstance.$el);
+}
 
-// Define typescript interfaces for installable component
-type InstallableComponent = typeof component & PluginObject<any>;
+const install: PluginObject<any> | PluginFunction<any> = {
+  install: () => {
+    Vue.prototype.$showModal = showModal;
+  }
+}
 
-// Default export is installable instance of component.
-// IIFE injects install function into component, allowing component
-// to be registered via Vue.use() as well as Vue.component(),
-export default /*#__PURE__*/((): InstallableComponent => {
-  // Assign InstallableComponent type
-  const installable = component as unknown as InstallableComponent;
-
-  // Attach install function executed by Vue.use()
-  installable.install = (Vue: typeof _Vue) => {
-    Vue.component('VueStandaloneModal', installable);
-  };
-  return installable;
-})();
-
-// It's possible to expose named exports when writing components that can
-// also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
-// export const RollupDemoDirective = directive;
+export default install;
