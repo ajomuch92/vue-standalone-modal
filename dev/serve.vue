21ddc736642616df70a33b66ced03a5170d076a4
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-12-31 09:58:17 -0600

    feat: 🎸 Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Vue from 'vue';
+import VueStandaloneModal from '@/vue-standalone-modal.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueStandaloneModal
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-standalone-modal />
+  </div>
+</template>
